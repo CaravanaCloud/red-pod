a52343c731d17f7d86c55282ec88cd91c490d91b
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-07-05 15:04:05 +0000

    wip

--- install-openshift-gitops.sh
+++ install-openshift-gitops.sh
@@ -8,3 +8,4 @@ sleep 5
 GITOPS_SERVER=$(oc get route -n openshift-gitops -o json | jq '.items[] | select(.metadata.name | contains("openshift-gitops-server")).spec.host' -r)
 echo GITOPS_SERVER=$GITOPS_SERVER
 # oc get secret openshift-gitops-cluster  -n openshift-gitops -o jsonpath="{.data.admin\.password}" | base64 -d
+# oc top pods   -n openshift-gitops
