8edd06adbf853f31f5c1ed112646b95eefc7840f
Julio Faerman <jufaerma@redhat.com>
2025-01-27 07:27:31 +0000

    wip

--- scripts/devbox_init_hook.sh
+++ scripts/devbox_init_hook.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR="$(dirname $DIR)"
 
-source $DIR/install-clients.
+source $DIR/install-clients.sh
 export DEVBOX_INIT_HOOK="success"
 echo "Devbox init hook successful"
